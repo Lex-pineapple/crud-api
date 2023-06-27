95d6c65984dccf9519fd898311744a7ff932cd4a
Lex <pineapplepear55@gmail.com>
2023-06-27 21:54:10 +0300

    feat: add 2nd test case

--- src/__tests__/api-e2e.test.ts
+++ src/__tests__/api-e2e.test.ts
@@ -78,4 +78,119 @@ describe('e2e tests', () => {
       expect(response.body).toMatchObject([]);
     });
   });
+  describe('case #2', () => {
+    const correctData = {
+      username: 'Mimi',
+      age: 25,
+      hobbies: ['barking'],
+    };
+    const wrongData1 = {
+      id: '56b28301-667e-4548-ae18-b9fb5dc7da49',
+      username: 'Mimi',
+      age: 25,
+      hobbies: ['barking'],
+    };
+    const wrongData2 = {
+      username: 12,
+      age: 999,
+      hobbies: 'baking',
+    };
+    let createdRecord: PaDB.IDBRecord;
+    let createdRecord2: PaDB.IDBRecord;
+    let updatedRecord: PaDB.IDBRecord;
+
+    test('should get empty db', async () => {
+      const response = await request(APILink).get(endpoint);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject([]);
+    });
+
+    test('should fail to create new object', async () => {
+      const response = await request(APILink).post(endpoint).send(wrongData1);
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toBe('Request body does not contain reqied fields');
+
+      const response2 = await request(APILink).post(endpoint).send(wrongData2);
+      expect(response2.statusCode).toBe(400);
+      expect(response2.body).toBe('Request body does not contain reqied fields');
+    });
+
+    test('should get empty db', async () => {
+      const response = await request(APILink).get(endpoint);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject([]);
+    });
+
+    test('should create new record', async () => {
+      const response = await request(APILink).post(endpoint).send(correctData);
+      createdRecord = response.body;
+      expect(response.statusCode).toBe(201);
+      expect(Object.keys(response.body).length).toBe(4);
+      expect(response.body.username).toBe(correctData.username);
+      expect(response.body.age).toBe(correctData.age);
+      expect(response.body.hobbies).toMatchObject(correctData.hobbies);
+    });
+
+    test('should fail to delete record', async () => {
+      const id = '6de54bfe-c0e5-4b54-aaf8-0e8f69a9d676';
+      const response = await request(APILink).delete(endpoint + '/' + id);
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toBe(`User with ID ${id} does not exist`);
+    });
+
+    test('should get newly created record', async () => {
+      const response = await request(APILink).get(endpoint + '/' + createdRecord.id);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(createdRecord);
+    });
+
+    test('should fail update record', async () => {
+      const response = await request(APILink)
+        .put(endpoint + '/' + createdRecord.id)
+        .send({
+          username: 'ADMIN',
+          age: 'IMORTAL',
+          hobbies: ['lying', 'scissors'],
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toBe('Request body does not contain reqied fields');
+    });
+
+    test('should update record', async () => {
+      const response = await request(APILink)
+        .put(endpoint + '/' + createdRecord.id)
+        .send({
+          username: 'ADMIN',
+          age: 999,
+          hobbies: ['lying', 'scissors'],
+        });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.username).toBe('ADMIN');
+      expect(response.body.age).toBe(999);
+      expect(response.body.hobbies).toMatchObject(['lying', 'scissors']);
+      updatedRecord = {
+        id: createdRecord.id,
+        username: 'ADMIN',
+        age: 999,
+        hobbies: ['lying', 'scissors'],
+      };
+    });
+
+    test('should create new record', async () => {
+      const response = await request(APILink).post(endpoint).send(correctData);
+      createdRecord2 = response.body;
+      expect(response.statusCode).toBe(201);
+      expect(Object.keys(response.body).length).toBe(4);
+      expect(response.body.username).toBe(correctData.username);
+      expect(response.body.age).toBe(correctData.age);
+      expect(response.body.hobbies).toMatchObject(correctData.hobbies);
+    });
+
+    test('should get db with 2 records', async () => {
+      const fullDb = [updatedRecord, createdRecord2];
+      const response = await request(APILink).get(endpoint);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject(fullDb);
+    });
+  });
 });
